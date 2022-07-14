36e4bef39508a63ee636cca5c09fb549b2047583
arambalayan <balayan.aram1995@gmail.com>
2022-07-14 11:01:25 +0400

    task2_xndir3

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -30,12 +31,30 @@ char* strset(char* s, char* ch){
     	return s;
 }
 
+void strmove(char* arr, int n){
+    int last, length = strlen(arr);  
+    for(int i = 0; i < n; i++){  
+        last = arr[length-1];  
+        for(int j = length-1; j > 0; j--){  
+            arr[j] = arr[j-1];  
+        }  
+        arr[0] = last;  
+    }  
+}
+
 int main() {
 
     char str1[] = "Aram";
     char str2[] = "Balayan";
     cout << concat(str1, str2) << endl;
+    
     char s[] = "Aram";
     char ch[] = "*";
     cout << strset(s, ch) << endl;
+    
+    char s1[] = "hello";
+    int n = 2;
+    strmove(s1, n);
+    cout << s1 << endl;
+    
 }
